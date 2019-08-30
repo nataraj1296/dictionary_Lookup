de190553347af102922a4b0e3a4b10a14feb9c02
nataraj1296 <43031124+nataraj1296@users.noreply.github.com>
2019-08-30 16:49:30 +0530

    Dictionary lookup in C# using Lists

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Dictionary_Lookup
+{
+    class Program
+    {
+        public static void Dict_Lookup(string textfile, string search)
+        {
+            //Create the dictionary list
+            List<string> words = System.IO.File.ReadAllText(textfile).Split(new string[] { "\n" }, StringSplitOptions.None).ToList();
+            
+            //Search the dictionary list
+            List<string> results = words.FindAll(delegate (string s) { return s.Contains(search); });
+            //Display the results
+            results.ForEach(i => Console.WriteLine("{0}", i));
+            //Wait for user to press key
+            Console.ReadKey();
+        }
+        static void Main(string[] args)
+        {
+            String path = "";
+            String src = "";
+            Console.WriteLine("Enter path for text file:");
+            path = Console.ReadLine();
+            Console.Write("Enter search word or part of word:");
+            src = Console.ReadLine();
+            Dict_Lookup(path,src);
+        }
+    }
+}
